579522f4b732f0cd7afea13b8a0d1ffe9b50ad20
unknown <londonvayavong@gmail.com>
2017-12-22 16:00:27 -0500

    first

---
+++ keys.js
@@ -0,0 +1,9 @@
+
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '78pphCyqFVnx7SXKhURIa5faA',
+  consumer_secret: '<input here>',
+  access_token_key: '944277607524323329-iNK1TIONaxwcQx7wk8Hp2k2UrkRuoBr',
+  access_token_secret: 'CYGOIf1y3Rls25UouyPDv48msxCRf2dd5gAG9U1VBuYiX',
+}
