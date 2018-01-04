a1c6fe3d1d8c753441e5f7b0872ef28e6086cb75
unknown <londonvayavong@gmail.com>
2018-01-03 20:33:28 -0800

    added twitter key and adjusted directions

--- keys.js
+++ keys.js
@@ -3,7 +3,7 @@ console.log('this is loaded');
 
 var twitterKeys = {
   consumer_key: '78pphCyqFVnx7SXKhURIa5faA',
-  consumer_secret: '<input here>',
+  consumer_secret: '3RpKBZDrusK5SMN0bqTXyepP47Y3e0s5K8BJ1Lk3T33GLEGFjO',
   access_token_key: '944277607524323329-iNK1TIONaxwcQx7wk8Hp2k2UrkRuoBr',
   access_token_secret: 'CYGOIf1y3Rls25UouyPDv48msxCRf2dd5gAG9U1VBuYiX',
 }
